5c58379bbd1fe4fa73ad060119cca4f4f6fded15
Julie Moon <8juliemoon@gmail.com>
2016-04-26 16:14:25 -0700

    finished 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+#SPECIES ------------------------
+#Canis
+#
+#CHARACTERISTICS ----------------
+#number of legs: 4
+#Friendly: yes
+#number of tails: 1
+#Eye count: 2
+#Name: varies
+#Color: varies
+#
+#BEHAVIOR -----------------------
+#Jump
+#Runs
+#Barks
+#Licks
+#Scamper
