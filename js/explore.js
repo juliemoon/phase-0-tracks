1981f2d7ab920bc7f629805357ff28172057badf
Julie Moon <8juliemoon@gmail.com>
2016-05-05 11:40:56 -0700

    created reverse function

---
+++ js/explore.js
@@ -0,0 +1,14 @@
+//RELEASE 4- REVERSE A STRING FUNCTION
+// DRIVER CODE:
+// CREATE A FUNCTION THAT WILL TAKE A STRING
+// THEN PRINTS OUT THE STRING IN REVERSE ORDER IN ANOTHER VAR
+
+function reverse(str){
+  var output = ""
+  for (var i = str.length-1; i>=0; i--){
+    output += str[i]
+  }
+  console.log(output)
+}
+
+console.log(reverse("hello"));
